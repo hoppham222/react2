ea252f9be900f3a34d8ffe9d4a3946f32e40d404
hoppham222 <hop98pham@gmail.com>
2022-02-22 19:59:27 +0700

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bắt đầu một loại mới
         </p>
         <a
           className="App-link"
